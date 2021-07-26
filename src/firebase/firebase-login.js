7c2f9c6ff8a25c8d0270107fb528ac09f9fcf8d0
Pierina Montalva <pierinamfatur@gmail.com>
2021-07-26 17:48:40 -0500

    Se agrego funcion para iniciar sesion

--- src/firebase/firebase-login.js
+++ src/firebase/firebase-login.js
@@ -3,20 +3,19 @@ import * as todo from './firebase-config.js';
 todo.firebaseConfig;
 
 // FUNCIÓN PARA REGISTRARSE
-export const getInputValue = () => {
-  const signUpBtn = document.querySelector('#signup-btn');// Llama a registrarse
-  signUpBtn.addEventListener('click', (e) => { /* Evento al pulsar */
-    // Evita que la página vuelva a cargar//
-    e.preventDefault();
-    // Llamando el valor de los inputs
-    const email = document.querySelector('#signup-email').value;
-    const password = document.querySelector('#signup-password').value;
+
+
+export const userSignUp = (email, password) => {
+    let result = false;
     //  Autentificación => Método de Firebase
     todo.auth
       .createUserWithEmailAndPassword(email, password)
       .then((userCredentials) => {
-        console.log('sign up');
-        console.log(userCredentials.operationType);
+        console.log(userCredentials);
+        if (userCredentials.operationType == 'signIn') {
+            alert('hola');
+            result = true;
+        }
       })
       .catch((error) => {
         console.log(error);
@@ -34,5 +33,33 @@ export const getInputValue = () => {
           alert('El correo ingresado ya está siendo utilizado, por favor, ingresa un correo válido');
         }
       });
-  });
+    return result;
+};
+
+
+// FUNCIÓN PARA INICIAR SESIÓN
+export const userSignIn = (email, password) => {
+    //  Autentificación => Método de Firebase
+    todo.auth
+      .signInWithEmailAndPassword(email, password)
+      .then((userCredentials) => {
+        console.log('iniciaste sesión');
+      })
+      .catch((error) => {
+        // console.log(error);
+        const errorCode = error.code;
+
+        // error por campos vacíos
+        if (errorCode === 'auth/invalid-email') {
+            alert('Por favor, complete los campos');
+        }
+        // error contraseña incorrecta
+        if (errorCode === 'auth/wrong-password') {
+          alert('Contraseña incorrecta, inténtelo de nuevo');
+        }
+        // error usuario no encontrado
+        if (errorCode === 'auth/user-not-found') {
+            alert('El correo que ingresó no está registrado, por favor, regístrece');
+        }
+      });
 };
--- src/main.js
+++ src/main.js
@@ -15,17 +15,17 @@ loginDiv.innerHTML = `
     <!--SECCION PRINCIPAL DEL LOGIN-->
 
     <div class="content-container">
-        <img class="illustration" src="../images/dog-walking.svg" alt="">
+        <img class="illustration" src="./images/dog-walking.svg" alt="">
         <form id="login-form">
-                <img class="logo" src="../images/petPlace.svg" alt="">
+                <img class="logo" src="./images/petPlace.svg" alt="">
                 <p>¡Bienvenid@ Pet Lover!</p>
                 <input type="email" placeholder="Coloca tu correo" id="email" required>
                 <input type="password" placeholder="Coloca tu contraseña" name="" id="password" required>
                 <button id="signin-btn">Iniciar Sesión</button>
                 <p>O bien ingresa por...</p>
                 <div class="options">
-                    <img class="img" id="facebook-btn" src="../images/facebook.svg" alt="">
-                    <img class="img" id="gmail-btn" src="../images/google.svg" alt="">
+                    <img class="img" id="facebook-btn" src="./images/facebook.svg" alt="">
+                    <img class="img" id="gmail-btn" src="./images/google.svg" alt="">
                 </div>
                 <p>¿No tienes cuenta? <strong id="sign-up">Regístrate</strong></p>
          </form>
@@ -35,7 +35,7 @@ loginDiv.innerHTML = `
 
     <div id="signup-container" style= "display: none">
         <form id="signup-form">
-                <img class="logo" src="../images/petPlace.svg" alt="">
+                <img class="logo" src="./images/petPlace.svg" alt="">
                 <h2>Regístrate gratis</h2>
                 <div class="labels-container">
                     <label for="signup-name">Coloca tu nombre:</label>
@@ -54,6 +54,7 @@ loginDiv.innerHTML = `
 
     `;
 loginSection.appendChild(loginDiv);
+
 // FUNCIÓN PARA MOSTRAR CONTENEDOR DE REGISTRO
 const contentContainer = document.querySelector('.content-container'); // Llama contenedor del login
 const signUpForm = document.querySelector('#sign-up'); // Llama a registrarse
@@ -62,20 +63,52 @@ signUpForm.addEventListener('click', (e) => {
   e.preventDefault();
   contentContainer.style.display = 'none';
   signUpContainer.style.display = 'block';
-  // Función para registrarse
-  all.getInputValue();
 });
+
 // FUNCIÓN PARA MOSTRAR CONTENEDOR DE INICIAR SESIÓN
 const signInForm = document.querySelector('#sign-in');
-const illustration = document.querySelector('.illustration');
-// const inputs = document.querySelectorAll('.input');
-// const active = document.activeElement;
-
 signInForm.addEventListener('click', (e) => {
   e.preventDefault();
-  contentContainer.style.display = 'block';
+  contentContainer.style.display = 'flex';
   signUpContainer.style.display = 'none';
-  illustration.style.display = 'none';
+  
   // Sirve para resetear el registro cuando haces click en el texto Iniciar Sesión
   document.querySelector('#signup-form').reset();
+});
+
+// FUNCION PARA REGISTRARSE
+const signUpBtn = document.querySelector('#signup-btn');// Llama a registrarse
+signUpBtn.addEventListener('click', (e) => { 
+    // Evita que la página vuelva a cargar//
+    e.preventDefault();
+
+    // Llamando el valor de los inputs
+    const email = document.querySelector('#signup-email').value;
+    const password = document.querySelector('#signup-password').value;
+
+   let result = all.userSignUp(email, password);
+   alert(result);
+    if (result === true) {
+        alert('entro')
+        const contentContainer = document.querySelector('.content-container');
+        const signUpContainer = document.getElementById('signup-container'); // Llama contenedor de registro
+
+        contentContainer.style.display = 'flex';
+        signUpContainer.style.display = 'none';
+    }
+    
+});
+
+// FUNCION PARA INICIAR SESION
+const signInBtn = document.querySelector('#signin-btn');// Llama boton de iniciar sesión
+
+signInBtn.addEventListener('click', (e) => { /* Evento al pulsar */
+    // Evita que la página vuelva a cargar//
+    e.preventDefault();
+
+    // Llamando el valor de los inputs
+    const email = document.querySelector('#email').value;
+    const password = document.querySelector('#password').value;
+
+    all.userSignIn(email, password);
 });
