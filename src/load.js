d26405bebac963b64da57804c84c100680131ba9
makemile <mkengya@gmail.com>
2021-08-17 08:40:23 -0500

    se eliminaron algunas documentos de la carpeta view

--- src/load.js
+++
@@ -1,7 +0,0 @@
-import { changesView } from "./view_controler/index.js"
-
-const init = () => {
-    window.addEventListener('hashchange', () => console.log(window.location.hash))
-}
-
-window.addEventListener('load', init)
--- src/view/index_obj.js
+++
@@ -1,9 +0,0 @@
-import Home from "./Home.js";
-import Profile from "./Profile.js";
-
-const items = {
-    home: Home,
-    profile: Profile,
-}
-
- export {items };
